02d44e728894ad536eef99a43f6e3f438d832646
root <molitdj97@gmail.com>
2020-10-30 17:28:58 -0400

    Skipping tests that are unable to run outside of local environment

--- tests/testthat/test_authorization_functions.R
+++ tests/testthat/test_authorization_functions.R
@@ -4,8 +4,9 @@ test_that("authorization functions work", {
   expect_null(set_id_secret())
   expect_invisible(set_id_secret())
   
+  skip("No API Keys to run test")
   expect_is(get_id_secret(), "list")
-  
+
   client_id <- get_id_secret()[[1]]
   client_secret <- get_id_secret()[[2]]
   expect_is(get_access_token(client_id, client_secret), "character")
--- tests/testthat/test_post_functions.R
+++ tests/testthat/test_post_functions.R
@@ -1,24 +1,25 @@
 context("post functions")
 
 test_that("post functions are working properly", {
+  skip("No API Keys to run test")
   client_id <- get_id_secret()[[1]]
   client_secret <- get_id_secret()[[2]]
   token <- get_access_token(client_id, client_secret)
-  
+
   txt <- "i am a welder and used welding equipment and acetylene torches"
-  expect_output(str(competencies(text = txt, 
+  expect_output(str(competencies(text = txt,
                                  bias = 0.4,
-                                 token = token)), 
+                                 token = token)),
                 "Valid request\nList of 9")
-  
+
   expect_output(str(competencies_flatten(text = txt, bias = 0.4,
-                                         token = token)), 
+                                         token = token)),
                 "Valid request\nList of 11")
-  
+
   expect_output(str(competencies_military(moc = "2A554E",
                                           token = token)),
                 "Valid request\nList of 12")
-  
+
   txt <- "Provide substance abuse education and counseling for at-risk individuals."
   expect_output(str(skills(text = txt,
                            depth = 20,
@@ -26,9 +27,9 @@ test_that("post functions are working properly", {
                            similarity_scoring = "true",
                            token = token)),
                 "Valid request\nList of 2")
-  
-  txts <- c("Weld things together", 
-            "Use an acetylene torch", 
+
+  txts <- c("Weld things together",
+            "Use an acetylene torch",
             "Hammer nails into wood")
   expect_output(str(skills_multi_match(sentences = txts,
                                        depth = 20,
@@ -39,8 +40,8 @@ test_that("post functions are working properly", {
                                                 "49-9071.00"),
                                        token = token)),
                 "Valid request\nList of 3")
-  
-  expect_output(str(topics_related(texts = c("ms word", "access", "dexterity"),                   
+
+  expect_output(str(topics_related(texts = c("ms word", "access", "dexterity"),
                                    socs = "41-3041.00",
                                    token = token)),
                 "Valid request\nList of 2")
