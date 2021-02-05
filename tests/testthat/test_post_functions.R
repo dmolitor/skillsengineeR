075d92e9b0181a8439d18dc960c574810371df6d
root <molitdj97@gmail.com>
2021-02-05 13:35:39 -0500

    Adding more detailed functionality and more robust tests

--- tests/testthat/test_post_functions.R
+++ tests/testthat/test_post_functions.R
@@ -5,44 +5,63 @@ test_that("post functions are working properly", {
   client_id <- get_id_secret()[[1]]
   client_secret <- get_id_secret()[[2]]
   token <- get_access_token(client_id, client_secret)
-
+  
+  # Test competencies
   txt <- "i am a welder and used welding equipment and acetylene torches"
-  expect_output(str(competencies(text = txt,
-                                 bias = 0.4,
-                                 token = token)),
-                "Valid request\nList of 9")
+  test <- competencies(text = txt,
+                       bias = 0.4,
+                       token = token)
+  expect_type(test,
+              "list")
+  expect_equal(length(test), 9)
 
-  expect_output(str(competencies_flatten(text = txt, bias = 0.4,
-                                         token = token)),
-                "Valid request\nList of 11")
+  # Test competencies_flatten
+  test <- competencies_flatten(text = txt,
+                               bias = 0.4,
+                               token = token)
+  expect_type(test,
+              "list")
+  expect_equal(length(test), 11)
 
-  expect_output(str(competencies_military(moc = "2A554E",
-                                          token = token)),
-                "Valid request\nList of 12")
+  # Test competencies_military
+  test <- competencies_military(moc = "2A554E",
+                                token = token)
+  expect_type(test,
+              "list")
+  expect_equal(length(test), 12)
 
+  # Test skills
   txt <- "Provide substance abuse education and counseling for at-risk individuals."
-  expect_output(str(skills(text = txt,
-                           depth = 20,
-                           cutoff = 5,
-                           similarity_scoring = "true",
-                           token = token)),
-                "Valid request\nList of 2")
+  test <- skills(text = txt,
+                 depth = 20,
+                 cutoff = 5,
+                 similarity_scoring = "true",
+                 token = token)
+  expect_type(test,
+              "list")
+  expect_equal(length(test), 2)
 
+  # Test skills_multi_match
   txts <- c("Weld things together",
             "Use an acetylene torch",
             "Hammer nails into wood")
-  expect_output(str(skills_multi_match(sentences = txts,
-                                       depth = 20,
-                                       cutoff = 5,
-                                       socs = c("17-2141.00",
-                                                "49-9041.00",
-                                                "49-1011.00",
-                                                "49-9071.00"),
-                                       token = token)),
-                "Valid request\nList of 3")
+  test <- skills_multi_match(sentences = txts,
+                             depth = 20,
+                             cutoff = 5,
+                             socs = c("17-2141.00",
+                                      "49-9041.00",
+                                      "49-1011.00",
+                                      "49-9071.00"),
+                             token = token)
+  expect_type(test,
+              "list")
+  expect_equal(length(test), 3)
 
-  expect_output(str(topics_related(texts = c("ms word", "access", "dexterity"),
-                                   socs = "41-3041.00",
-                                   token = token)),
-                "Valid request\nList of 2")
+  # Test topics_related
+  test <- topics_related(texts = c("ms word", "access", "dexterity"),
+                         socs = "41-3041.00",
+                         token = token)
+  expect_type(test,
+              "list")
+  expect_equal(length(test), 2)
 })
